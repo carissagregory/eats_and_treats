205cb200f5be127c2816179ed0fbfbadbc1d02e1
Carissa Gregory <carissagregory@Carissas-MacBook-Air.local>
2024-11-21 16:14:18 -0600

    initial commit

---
+++ js/eatsAndTreatsJS.js
@@ -0,0 +1,200 @@
+//eats and treats javascript
+
+//display current copyright year
+let currentYear = new Date();
+
+  function currentDate(currentYear) {
+	let copyrightYear = currentYear.getFullYear();
+	document.querySelector(".copyrightYear").innerHTML = copyrightYear;
+}
+
+currentDate(currentYear);
+
+//defining recipe obejct
+let recipeObject = [];
+
+//submit a recipe form validation
+//  get form values
+let recipeName = document.querySelector("#recipeName");
+let recipeAuthor = document.querySelector("#recipeAuthor");
+let recipeNumServings = document.querySelector("#recipeNumServings");
+let recipeServingType = document.querySelector("#recipeServingType");
+let recipeCookingTime = document.querySelector("#recipeCookingTime");
+let recipeDifficulty = document.querySelector("#recipeDifficultyLevel");
+let recipeCategory = document.querySelectorAll("input[name='recipeCategory']");
+let recipeDescription = document.querySelector("#recipeDescription");
+let inRecipeImageName = document.querySelector("#inRecipeImageName");
+let inRecipeImage = document.querySelector("#inRecipeImage");
+
+let ingredientCounter = 2;
+
+function addIngredientFields() {
+    //display a set of ingredient fields when use clicks ingredient button
+    //  access the template object
+    //  create a counter to update the for, name, and id attributed on the measurement, volume, and ingredient fields
+    //  assign variables for the measurement, volume, and ingredient fields through the templateObject
+    //  append fields to ingredient section
+    //  update counter after appending
+    //alert("inside addIngredientFields()");
+    let templateObject = document.querySelector("#ingredientTemplate"); 
+    let addIngredientFields = templateObject.content.cloneNode(true); 
+    //console.log(addIngredientFields);
+
+    /* 
+    let updatePId = addIngredientFields.querySelector("#ingredient1");
+    updatePId.id = "ingredient" + counter;
+    //console.log(updatePId);
+    */
+    let updateMeasurementFor = addIngredientFields.querySelector("label[for='ingredient1Measurement']");
+    updateMeasurementFor.htmlFor = "ingredient" + ingredientCounter + "Measurement";
+    let updateMeasurementField = addIngredientFields.querySelector("#ingredient1Measurement");
+    updateMeasurementField.id = "ingredient" + ingredientCounter + "Measurement";
+    updateMeasurementField.name = "ingredient" + ingredientCounter + "Measurement";
+   //console.log(addIngredientFields);
+
+   let updateVolumeFor = addIngredientFields.querySelector("label[for='ingredient1Volume']");
+   updateVolumeFor.htmlFor = "ingredient" + ingredientCounter + "Volume";   
+   let updateVolumeField = addIngredientFields.querySelector("#ingredient1Volume");
+   updateVolumeField.id = "ingredient" + ingredientCounter + "Volume";
+   updateVolumeField.name = "ingredient" + ingredientCounter + "Volume";
+
+   let updateTypeFor = addIngredientFields.querySelector("label[for='ingredient1Type']");
+   updateTypeFor.htmlFor = "ingredient" + ingredientCounter + "Type";   
+   let updateTypeField = addIngredientFields.querySelector("#ingredient1Type");
+   updateTypeField.id = "ingredient" + ingredientCounter + "Type";
+   updateTypeField.name = "ingredient" + ingredientCounter + "Type";
+   //console.log(addIngredientFields);
+
+   document.querySelector("#ingredientsFields").appendChild(addIngredientFields);
+    
+   ingredientCounter++;
+}    
+
+let directionCounter = 2;
+
+function addDirectionField() {
+    
+    //display direction fields when use clicks direction button
+    //  access the template object
+    //  create a counter to update the for, name, and id attributed on the measurement, volume, and ingredient fields
+    //  assign variables for the measurement, volume, and ingredient fields through the templateObject
+    //  append fields to direction section
+    //  update counter after appending
+    //alert("inside addDirectionField()");
+    let templateObject = document.querySelector("#directionTemplate"); 
+    let addDirectionFields = templateObject.content.cloneNode(true); 
+
+    let updateDirectionLabel = addDirectionFields.querySelector("label[for='direction1']");
+    updateDirectionLabel.htmlFor = "direction" + directionCounter;
+    updateDirectionLabel.innerHTML = directionCounter + ".";
+
+    let updateDirectionNumber = addDirectionFields.querySelector("#direction1");
+    updateDirectionNumber.id = "direction" + directionCounter;
+    updateDirectionNumber.name = "direction" + directionCounter;
+    //console.log(updateDirectionNumber);
+
+    document.querySelector("#directionsFields").appendChild(addDirectionFields);
+    
+    directionCounter++;
+}
+
+function validateRecipeFormFields() {
+//add recaptcha to form
+    //check that fields are not blank and text fields do not have numbers and cooking time should not have decimals
+    //if all fields completed then submit the form
+        //before submitting the form input all form data into a recipe object or recipe class
+        //clear form fields and display confirmation message
+    //else form should not submit and error message is displayed
+    //alert("insideValidateRecipeFormFields()");
+
+    //check if one of the recipeCategory radioButtons is selected
+    let selectedRadioButton = false;
+    recipeCategory.forEach (radioButton => {
+        if (radioButton.checked) {
+            selectedRadioButton = true;
+        }
+    });
+
+    //check if all of the ingredient fields are not blank
+    //  get both dynamically created and hardcoded fields after the template has been created
+    let ingredientMeasurement = document.querySelectorAll("input[id$='Measurement']");
+    let ingredientVolume = document.querySelectorAll("select[id$='Volume']");
+    let ingredientType = document.querySelectorAll("input[id$='Type']");
+    let measurementFields = true;
+    ingredientMeasurement.forEach (measurement => {
+        if (measurement.value.trim() == "") {
+            measurementFields = false;
+        }
+    });
+    //console.log(ingredientMeasurement);
+
+    let volumeFields = true;
+    ingredientVolume.forEach(volume => {
+        if (volume.value.trim() == "") {
+            volumeFields = false;
+        }
+    });
+
+    let typeFields = true;
+    ingredientType.forEach(type => {
+        if (type.value.trim() == "") {
+            typeFields = false;
+        }
+    });
+
+    //check if all of the direction fields are not blank
+    let directionTextarea = document.querySelectorAll("textarea[id^='direction']");
+    let directionFields = true;
+    directionTextarea.forEach(direction => {
+        if (direction.value.trim() == "") {
+            directionFields = false;
+        }
+    });
+
+    if (
+        recipeName.value.trim() != "" && 
+        recipeAuthor.value.trim() != "" && 
+        recipeNumServings.value.trim() != "" && 
+        recipeServingType.value.trim() != "" &&
+        recipeCookingTime.value.trim() != "" &&
+        recipeDifficulty.value.trim() != "" &&
+        selectedRadioButton &&
+        recipeDescription.value.trim() != "" &&
+        measurementFields &&
+        volumeFields &&
+        typeFields &&
+        directionFields &&
+        inRecipeImageName.value.trim() != "" &&
+        inRecipeImage.files.length != 0
+        ){
+        alert("form fields are NOT blank");
+        //gather values from the form fields 
+        //send form value into the recipe object
+        //  define inputRecipe object
+        //  fill the measurements, volumnes, types, and directions fields using push
+        //  once all the values are gathered push inputRecipe object into the recipe arrayObject
+        //  use the JSON.Stringify to conver the js object to a JSON object
+        //reset all form fields 
+        //run the submit method
+        let inputRecipe = {
+            name: recipeName.value.trim(),
+            author: recipeAuthor.value.trim(), 
+            numServings: recipeNumServings.value.trim(),
+            servingType: recipeServingType.value.trim(), 
+            cookingTime: recipeCookingTime.value.trim(),
+            difficulty: recipeDifficulty.value.trim(),
+            category: "",
+            description: "",
+            measurements: [],
+            volumes: [],
+            types: [],
+            directions: [],
+            imageName: "",
+            image: ""
+        }
+    }
+    else {
+        alert("form fields are blank")
+    }
+
+}
---
+++ php/recipeFormHandler.php
@@ -0,0 +1,29 @@
+<?php
+//
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--css-->
+    <link href="../css/styles.css" rel="stylesheet" type="text/css">
+    <!--headings font-->
+    <link href="https://fonts.googleapis.com/css2?family=Mate+SC&display=swap" rel="stylesheet">
+    <!--regular text font-->
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+    <!--logo-->
+    <link rel="icon" href="../images/eatsAndTreatsLogo.png" type="image/png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Submit Recipe Confirmation</title>
+</head>
+<body class="confirmationContainer">
+    <div id="submitRecipeConfirmation">
+        <h1>Thank you for submitting a recipe!</h1>
+        <p>
+            Your recipe should appear on the site shortly. 
+            If your recipe has not shown up within 24 hours please let us know by submitting our contact form.
+        </p>
+        <button><a href="../index.html">Home Page</a></button>
+    </div>
+</body>
+</html>
