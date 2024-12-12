a79ed2fae85db4350d7e8bb8e787a4568f333585
Carissa Gregory <carissagregory@Carissas-MacBook-Air.local>
2024-12-12 12:37:37 -0600

    add mobile navigation button function

--- js/eatsAndTreatsJS.js
+++ js/eatsAndTreatsJS.js
@@ -9,3 +9,16 @@ let currentYear = new Date();
 }
 
 currentDate(currentYear);
+
+let nav = document.querySelector(".navigationBar nav");
+
+function displayMobileNav() {
+	//alert("inside displayMobileNav()");
+	//console.log(nav);
+	if (nav.style.display === "block") {
+		nav.style.display = "none";
+	}
+	else {
+		nav.style.display = "block"
+	}
+}
